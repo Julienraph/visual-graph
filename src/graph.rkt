dca65ca9332e7c53de6fefb62107550db7052836
Alessandro Rubio <rubmoa@gmail.com>
2017-11-02 21:54:07 +0100

    Creation du module graph.rkt

---
+++ src/graph.rkt
@@ -0,0 +1,43 @@
+#lang racket
+;;; vect2D.rkt
+;;; DrRacket 6.10
+;;; Module offrant des fonctionalités élémentaires
+;;; sur les graphes et basé sur une representation
+;;; par liste adjacence.
+;;; Pour le projet "Visualisation de graphe"
+
+(provide (all-defined-out))
+
+; Creation d'un graphe vide
+(define (empty-graph)
+  (make-hash))
+
+; Ajout d'un sommet node-id au graphe graph
+(define (add-node! graph node-id)
+  (hash-set! graph node-id '()))
+
+; TODO: Verifier qu'il existe node-id1 et node-id2
+; S'il existe pas on cree les deux sommets avec une arete
+; Si l'arete existe deja on modifie pas le graphe
+; Probleme: Comment parcourir le graphe pour verifier
+; l'existence d'un sommet specifique? 
+(define (add-edge! graph node-id1 node-id2)
+  (hash-set! graph node-id1 (list node-id2))
+  (hash-set! graph node-id2 (list node-id1)))
+
+; Sommet de graph
+; TODO: Le type de donné a renvoyer doit etre une liste
+(define (get-nodes graph)
+  (for ([(k v)(in-hash graph)])
+    (printf "~a " k)))
+
+; sommets voisins
+; ???
+(define (get-neighbors graph node-id)
+  (hash-ref graph node-id))
+
+
+; Variables de test
+
+(define g(empty-graph))
+(define v(empty-graph))
