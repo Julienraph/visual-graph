89a30c73c525a88ca6e2aadc16174e713d356401
Alessandro Rubio Moraes <rubmoa@gmail.com>
2017-11-13 10:05:21 +0100

    Base de module relaxation

--- src/relaxation.rkt
+++ src/relaxation.rkt
@@ -3,13 +3,14 @@
 ;;; DrRacket 6.10
 ;;; Projet "Visualisation de graphe"
 (require "positioning.rkt" "vect2D.rkt")
+
 ;; c2 = d
 ;; c3 = d^2
 (define relaxator%
   (class object%
 
-    ;; d = 0
-    (define-values (relax-c1 relax-c2 relax-c3 relax-c4)(values 2 0 0 0.1))
+    ;; d = 1
+    (define-values (relax-c1 relax-c2 relax-c3 relax-c4)(values 2 1 1 0.1))
 
     ;; setters
     (define/public (set-c1 n)
@@ -31,10 +32,23 @@
     (define/public (get-c4)
       relax-c4)
 
-    (super-new))
-    
-    
-  
-  
+    (define/public (force-m positioning node-id1 node-id2)
+      (when (and (hash-has-key? positioning node-id1) (hash-has-key? positioning node-id2))
+        (let ([x (hash-ref node-id1)]
+              [y (hash-ref node-id2)])
+          (define u (vect-unit (vect-mult x y)))
+          (define d (vect-norm (vect-mult x y)))
+          (define k (* relax-c1 (log (/ d relax-c2))))
+          (define res (vect-scalar k u))
+          res)))
 
-
+    (define/public (force-e positioning node-id1 node-id2)
+      (when (and (hash-has-key? positioning node-id1) (hash-has-key? positioning node-id2))
+        (let ([x (hash-ref positioning node-id1)]
+              [y (hash-ref positioning node-id2)])
+          (define u (vect-unit (vect-mult x y)))
+          (define d (sqr (vect-norm (vect-mult x y))))
+          (define k (* -1(/ relax-c3 d)))
+          (define res (vect-scalar k u))
+          res)))
+   (super-new)
