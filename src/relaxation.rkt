86453b77a11299abb11148fda480e90d6c00c245
Alessandro Rubio <rubmoa@gmail.com>
2017-11-13 17:02:54 +0100

    Objet soft

--- src/relaxation.rkt
+++ src/relaxation.rkt
@@ -4,51 +4,56 @@
 ;;; Projet "Visualisation de graphe"
 (require "positioning.rkt" "vect2D.rkt")
 
-;; c2 = d
-;; c3 = d^2
-(define relaxator%
-  (class object%
+(define (new-relaxator)
+  (let ([relax-c1 2 ]
+        [relax-c2 1]
+        [relax-c3 1]
+        [relax-c4 0.1])
+    (define (this methode . args)
+      (case methode
 
-    ;; d = 1
-    (define-values (relax-c1 relax-c2 relax-c3 relax-c4)(values 2 1 1 0.1))
+        ;; setters
+        ((set-c1)(set! relax-c1 (car args)))
+        ((set-c2)(set! relax-c2 (car args)))
+        ((set-c3)(set! relax-c3 (car args)))
+        ((set-c4)(set! relax-c4 (car args)))
 
-    ;; setters
-    (define/public (set-c1 n)
-      (define relax-c1 n))
-    (define/public (set-c2 n)
-      (define relax-c2 n))
-    (define/public (set-c3 n)
-      (define relax-c3 n))
-    (define/public (set-c4 n)
-      (define relax-c4 n))
-
-    ;; getters
-    (define/public (get-c1)
-      relax-c1)
-    (define/public (get-c3)
-      relax-c2)
-    (define/public (get-c3)
-      relax-c3)
-    (define/public (get-c4)
-      relax-c4)
-
-    (define/public (force-m positioning node-id1 node-id2)
-      (when (and (hash-has-key? positioning node-id1) (hash-has-key? positioning node-id2))
-        (let ([x (hash-ref node-id1)]
-              [y (hash-ref node-id2)])
+        ;; getters
+        ((get-c1) relax-c1)
+        ((get-c2) relax-c2)
+        ((get-c3) relax-c3)
+        ((get-c4) relax-c4)
+        
+        ;; Force mecanique
+        ((force-m)
+         (when (and (hash-has-key? (first args) (second args))(hash-has-key? (first args)(third args)))
+            (let ([x (hash-ref (first args) (second args))]
+              [y (hash-ref (first args) (third args))])
           (define u (vect-unit (vect-mult x y)))
-          (define d (vect-norm (vect-mult x y)))
-          (define k (* relax-c1 (log (/ d relax-c2))))
+          (define d (sqr (vect-norm (vect-mult x y))))
+          (define k (* -1 (/ relax-c3 d)))
           (define res (vect-scalar k u))
           res)))
-
-    (define/public (force-e positioning node-id1 node-id2)
-      (when (and (hash-has-key? positioning node-id1) (hash-has-key? positioning node-id2))
-        (let ([x (hash-ref positioning node-id1)]
-              [y (hash-ref positioning node-id2)])
+        
+        ;; Force electrique
+        ((force-e)
+         (when (and (hash-has-key? (first args) (second args))(hash-has-key? (first args)(third args)))
+            (let ([x (hash-ref (first args) (second args))]
+              [y (hash-ref (first args) (third args))])
           (define u (vect-unit (vect-mult x y)))
           (define d (sqr (vect-norm (vect-mult x y))))
           (define k (* -1(/ relax-c3 d)))
           (define res (vect-scalar k u))
           res)))
-   (super-new)
+
+        (else (error "Unknown method: " methode))))this))
+        
+        
+           
+           
+                    
+
+        
+        
+        
+  
